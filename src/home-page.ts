5fcacdca85c7e7f2ea35ad6c3328e5827ebb7164
Michael Greason <mgreason18@gmail.com>
2023-06-22 13:20:34 -0400

    contunuing typescript fundamentals

--- src/home-page.ts
+++ src/home-page.ts
@@ -1,7 +1,10 @@
-let message : string = "Home Page"
-console.log(message)
 
-let age: number = 20
-if (age <50)
-    age += 10
-    console.log(age)
+let sales = 123_456_789
+let course = "TypeScript"
+let is_published = true
+let level
+
+
+function render(document) {
+    console.log(document)
+}
